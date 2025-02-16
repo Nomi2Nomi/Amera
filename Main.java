028370c4d9aefc7afa5c325136e5a3359d9e8782
Nomi2Nomi <82378246+Nomi2Nomi@users.noreply.github.com>
2025-02-16 14:30:25 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+]public class Main {
     public static void main(String[] args) {
         product shoes1 = new product(001, "Mary Janes blue", "Shoes", "36.50$", "New Winter Collection", "url");
         product shoes2 = new product(002, "Mary Janes black", "Shoes", "38.50$", "New Winter Collection", "url2");
